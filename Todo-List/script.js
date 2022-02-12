9ebfad9b895a8bec3c8bd1ffb106180989194c20
Pepzini <jideniniola@gmail.com>
2022-02-12 21:27:37 +0100

    Clean up code

--- Todo-List/script.js
+++ Todo-List/script.js
@@ -1,39 +1,65 @@
 //personal task add different colors to each text
-const form = document.getElementById('form');
-const input = document.getElementById('input');
-const todosList = document.getElementById('todos');
-const deleteTodo= document.getElementById('delete-icon')
+const form = document.getElementById("form");
+const input = document.getElementById("input");
+const todosList = document.getElementById("todos");
+const deleteTodo = document.getElementById("delete-icon");
 
-form.addEventListener('submit', (event) =>{
-  event.preventDefault()
+const todos = JSON.parse(localStorage.getItem('todos'));
 
-  addToDo()
-})
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  addToDo();
+});
 
 function addToDo(todo) {
-    let todoText = input.value;
+  let todoText = input.value;
 
-    if (todo) {
-        todoText = todo.text;
+  if (todo) {
+    todoText = todo.text;
+  }
+  if (todoText) {
+    const todoElement = document.createElement("div");
+    const span = document.createElement("SPAN");
+    const txt = document.createElement("i");
+    txt.classList.add("fas", "fa-trash-alt");
+    todoElement.classList.add("todo-item");
+
+    if (todo && todo.completed) {
+      todoElement.classList.add("completed");
     }
-     if (todoText) {
-       const todoElement = document.createElement('div');
-       todoElement.classList.add('todo-item')
-
-       if (todo && todo.completed) {
-          todoElement.classList.add('completed') 
-       }
-       todoElement.innerHTML = `
+    todoElement.innerHTML = `
        <li>${todoText}</li>
-        <i class="fas fa-trash-alt" id="delete-icon"></i>
-       `
-     
-        todoElement.addEventListener('click', () => {
-          todoElement.classList.toggle('completed');
-        
-      }) 
-       todosList.appendChild(todoElement);
+       `;
 
-       input.value = '';
-     }
+    todoElement.addEventListener("click", () => {
+      todoElement.classList.toggle("completed");
+      updateLS();
+    });
+
+    span.addEventListener("click", () => {
+      todosList.removeChild(todoElement);
+      updateLS();
+    });
+
+    todosList.appendChild(todoElement);
+    span.appendChild(txt);
+    todoElement.appendChild(span);
+
+    input.value = "";
+    updateLS();
+  }
+}
+function updateLS() {
+  todoElement = document.querySelectorAll('li')
+
+  const todos = []
+
+  todoElement.forEach(todoElement => {
+      todos.push({
+          text: todoElement.innerText,
+          completed: todoElement.classList.contains('completed')
+      })
+  })
+
+  localStorage.setItem('todos', JSON.stringify(todos))
 }
